67d69099b0fa09b74da9ace5b5136b87c2bc1932
Ellisande <justin.dragos@gmail.com>
2016-04-28 17:24:13 -0700

    Added a component for bright boxes.

---
+++ src/js/components/brightBox.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class BrightBox extends Component {
+  constructor(props){
+    super(props);
+    this.type = this.props.type || 'primary';
+  }
+  render(){
+    return (
+      <div className={`bright-box ${this.type}`}>
+        <div className={`bright-box-title ${this.type}`}>{this.props.title}</div>
+        <div className={`bright-box-content ${this.type}`}>
+          {this.props.children}
+        </div>
+      </div>);
+  }
+}
+
+export default BrightBox;
