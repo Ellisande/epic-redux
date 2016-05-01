6e85dc2fec360761a7b5a20aed1a9c29aff93abf
Ellisande <justin.dragos@gmail.com>
2016-05-01 14:18:11 -0700

    Added an optional bubble section to the bright box.

--- src/js/components/brightBox.jsx
+++ src/js/components/brightBox.jsx
@@ -6,9 +6,16 @@ class BrightBox extends Component {
     this.type = this.props.type || 'primary';
   }
   render(){
+    const onSubmit = this.props.onSubmit || (() => {});
+    const bubbleMarkup = (
+      <form className={`bright-box-bubble ${this.type}`} onSubmit={onSubmit}>
+        {this.props.bubble}
+      </form>);
+    const showBubble = this.props.bubble ? bubbleMarkup : undefined;
     return (
       <div className={`bright-box ${this.type} ${this.props.className}`}>
         <div className={`bright-box-title ${this.type}`}>{this.props.title}</div>
+        {showBubble}
         <div className={`bright-box-content ${this.type}`}>
           {this.props.children}
         </div>
