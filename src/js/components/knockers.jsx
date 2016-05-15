a6f72c6e9806a0454f1ee056354c27ffdb121a5b
Ellisande <justin.dragos@gmail.com>
2016-05-15 09:04:51 -0700

    Added buttons for approving and rejectin gknockers.

--- src/js/components/knockers.jsx
+++ src/js/components/knockers.jsx
@@ -1,14 +1,29 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import BrightBox from './brightBox';
+import {approveKnocker, rejectKnocker} from '../actions';
+import {dispatch} from '../services/socket';
 
 class Knockers extends Component {
   constructor(props){
     super(props);
   }
+  approveKnocker(knocker){
+    dispatch(approveKnocker(knocker.id));
+  }
+  rejectKnocker(knocker){
+    dispatch(rejectKnocker(knocker.id));
+  }
   render(){
     const showKnocking = this.props.knockers.length > 0 ? {} : {display: 'none'};
-    const mapKnocker = knocker => (<div className='knocker' key={knocker.id}>{knocker.message}</div>);
+    const mapKnocker = knocker => (
+      <div className='knocker' key={knocker.id}>
+        <div className='knock-message'>{knocker.message}</div>
+        <div className='knock-actions'>
+          <button className='fa fa-check' onClick={this.approveKnocker.bind(this, knocker)} />
+          <button className='fa fa-ban' onClick={this.rejectKnocker.bind(this, knocker)} />
+        </div>
+      </div>);
     return (
       <BrightBox type='tertiary' title='Knocking' style={showKnocking}>
         <div className='knockers'>
