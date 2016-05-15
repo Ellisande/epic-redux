79b647c6310370e6104c7c021af778146ebe8364
Ellisande <justin.dragos@gmail.com>
2016-05-15 00:40:07 -0700

    Added a slider for allowing knocking.

--- src/js/components/lock.jsx
+++ src/js/components/lock.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import BrightBox from './brightBox';
-import {setLocked, setNewHosts} from '../actions';
+import {setLocked, setNewHosts, allowKnocking, disableKnocking} from '../actions';
 import {findUser} from '../store/utils';
 import {dispatch} from '../services/socket';
 import _ from 'lodash';
@@ -11,6 +11,7 @@ class Lock extends Component {
     super(props);
     this.toggleLocked = this.toggleLocked.bind(this);
     this.toggleNewHosts = this.toggleNewHosts.bind(this);
+    this.toggleKnock = this.toggleKnock.bind(this);
   }
   toggleLocked(e){
     e.preventDefault();
@@ -19,6 +20,13 @@ class Lock extends Component {
   toggleNewHosts(e){
     e.preventDefault();
     dispatch(setNewHosts(!this.props.newHosts));
+  }
+  toggleKnock(e){
+    e.preventDefault();
+    if(this.props.allowKnocking){
+      return dispatch(disableKnocking());
+    }
+    return dispatch(allowKnocking());
   }
   render(){
     const boxStyle = this.props.host ? {} : {display: 'none'};
@@ -29,6 +37,13 @@ class Lock extends Component {
          <div className='toggle-wrapper'>
            <input type='checkbox' className='toggle' checked={this.props.locked} readOnly/>
            <label onClick={this.toggleLocked} />
+         </div>
+       </div>
+       <div className='knock-toggle'>
+         <label>Allow Knocks</label>
+         <div className='toggle-wrapper'>
+           <input type='checkbox' className='toggle' checked={this.props.allowKnocking} readOnly/>
+           <label onClick={this.toggleKnock} />
          </div>
        </div>
        <div className='new-host-toggle' style={{display: 'none'}}>
@@ -47,7 +62,8 @@ const selector = state => {
   return {
     host: _.get(user, 'host', false),
     newHosts: state.newHosts,
-    locked: state.locked
+    locked: state.locked,
+    allowKnocking: state.allowKnocking
   };
 };
 export default connect(selector)(Lock);
