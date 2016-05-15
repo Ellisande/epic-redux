b0258edb85786450b87291b9ad16eb4f5e1cabf9
Ellisande <justin.dragos@gmail.com>
2016-05-14 17:45:06 -0700

    Reducers for updating the meeting summaries.

--- src/js/store/reducers/meetings.js
+++ src/js/store/reducers/meetings.js
@@ -1,4 +1,5 @@
 import {ActionHandler, handleActions} from '../actionHandler';
+import {replace} from '../utils';
 
 const createMeeting = new ActionHandler('CREATE_MEETING', (meetings, action) => {
   return [...meetings, {name: action.name, participants: 0}];
@@ -8,7 +9,29 @@ const setMeetings = new ActionHandler('SET_MEETINGS', (meetings, action) => {
   return action.meetings;
 });
 
-const actions = [createMeeting, setMeetings];
+const growMeeting = new ActionHandler('GROW_MEETING', (meetings, action) => {
+  const currentMeeting = meetings.find(meeting => meeting.name === action.name);
+  if(!currentMeeting){
+    return meetings;
+  }
+  const updatedMeeting = Object.assign({}, currentMeeting, {participants: currentMeeting.participants + 1});
+  return replace(meetings, currentMeeting, updatedMeeting);
+});
+
+const shrinkMeeting = new ActionHandler('SHRINK_MEETING', (meetings, action) => {
+  const currentMeeting = meetings.find(meeting => meeting.name === action.name);
+  if(!currentMeeting){
+    return meetings;
+  }
+  const updatedMeeting = Object.assign({}, currentMeeting, {participants: currentMeeting.participants - 1});
+  return replace(meetings, currentMeeting, updatedMeeting);
+});
+
+const deleteMeeting = new ActionHandler('DELETE_MEETING', (meetings, action) => {
+  return meetings.filter(meeting => meeting.name !== action.name);
+});
+
+const actions = [createMeeting, setMeetings, growMeeting, shrinkMeeting, deleteMeeting];
 
 const meetingsReducer = (meetings = [], action) => {
   const newMeetings = handleActions(actions, meetings, action);
