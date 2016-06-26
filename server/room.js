95671382ee9f7380e173125db4dd5983da668c8c
Ellisande <justin.dragos@gmail.com>
2016-06-26 11:47:12 -0700

    Added middleware to the store creation for the room.

--- server/room.js
+++ server/room.js
@@ -1,20 +1,22 @@
 import {createStore} from '../shared/store/store';
+import {applyMiddleware} from 'redux';
+import {rejectMiddleware, logIt} from '../shared/store/middleware';
 
 class Room {
-  constructor(roomName){
+  constructor(roomName, ...middleware){
     this.sparks = [];
-    this.store = createStore({roomName: roomName});
+    this.store = createStore({roomName: roomName}, applyMiddleware(...rejectMiddleware, ...middleware, logIt));
     this.roomName = roomName;
   }
   send(action){
     this.sparks.forEach(spark => spark.write(action));
   }
   dispatch(action){
-    this.store.dispatch(action);
+    return this.store.dispatch(action);
   }
   dispatchAndSend(action){
-    this.dispatch(action);
-    this.send(action);
+    const dispatchedAction = this.dispatch(action);
+    this.send(dispatchedAction);
   }
   addSpark(spark){
     if(!spark){
