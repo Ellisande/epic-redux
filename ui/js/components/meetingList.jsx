500417d6097178a1019fe92f4c7475fe1c224d20
Ellisande <justin.dragos@gmail.com>
2016-09-08 17:09:02 -0700

    Added a filter to the meeting list.

--- ui/js/components/meetingList.jsx
+++ ui/js/components/meetingList.jsx
@@ -1,10 +1,24 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router';
+import {filterMeetings} from '../../../shared/actions';
 
 class MeetingList extends Component {
+  componentWillMount(){
+    const filterAction = filterMeetings();
+    this.props.dispatch(filterAction);
+  }
   render(){
     const {meetings} = this.props;
+    const filteredMeetings = meetings.filter(meeting => {
+      const filter = this.props.meetingFilter;
+      if(filter){
+        return meeting.name.startsWith(filter);
+      }
+      else {
+        return true;
+      }
+    });
     const meetingItem = (meeting) => {
       return (
       <div className='meeting-summary' key={meeting.name}>
@@ -16,7 +30,7 @@ class MeetingList extends Component {
     return (
       <div className='meeting-list'>
         <div className='meeting-summary'>Meetings: </div>
-        {meetings.map(meetingItem)}
+        {filteredMeetings.map(meetingItem)}
       </div>
     );
   }
