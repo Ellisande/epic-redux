cd339a0cbb4c8b68f720e401f03c1fcd0b5bf35e
Ellisande <justin.dragos@gmail.com>
2017-04-01 22:08:11 -0700

    Clean up reducers

--- shared/store/store.js
+++ shared/store/store.js
@@ -5,37 +5,17 @@ import {
 
 import participantReducer from './reducers/participants';
 import meetingReducer from './reducers/meetings';
-import phaseReducer from './reducers/phase';
-import lockedReducer from './reducers/locked';
-import newHostsReducer from './reducers/newHosts';
 import userReducer from './reducers/user';
 import topicsReducer from './reducers/topics';
 import roomNameReducer from './reducers/roomName';
-import lockedOutReducer from './reducers/lockedOut';
-import allowKnockingReducer from './reducers/allowKnocking';
-import knockersReducer from './reducers/knockers';
-
 
 let reducers = combineReducers({
   meetings: meetingReducer,
-  meetingFilter: (meetingFilter = '', action) => {
-    if(action.type === 'FILTER_MEETINGS'){
-      return action.filter || '';
-    }
-    return meetingFilter;
-  },
   participants: participantReducer,
   userId: userReducer,
-  phase: phaseReducer,
   roomName: roomNameReducer,
-  timer: (timer = 350000) => timer,
   phaseVotes: (phaseVotes = []) => phaseVotes,
   topics: topicsReducer,
-  locked: lockedReducer,
-  newHosts: newHostsReducer,
-  lockedOut: lockedOutReducer,
-  allowKnocking: allowKnockingReducer,
-  knockers: knockersReducer
 });
 
 const attachDevTools = () => {
