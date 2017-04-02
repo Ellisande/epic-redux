cc1b3f03a67ed4974cd7eafe643d21e1f4f0f5f0
Ellisande <justin.dragos@gmail.com>
2017-04-01 21:37:15 -0700

    Removed unused pages.

--- ui/js/main.jsx
+++ ui/js/main.jsx
@@ -1,18 +1,11 @@
 
-import {browserHistory, Router, Route, IndexRoute} from 'react-router';
+import {browserHistory, Router, Route} from 'react-router';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import injectTap from 'react-tap-event-plugin';
-import App from './components/app';
 import Meeting from './components/meeting';
-import MeetingList from './components/meetingList';
-import How from './components/how';
-import When from './components/when';
-import Safety from './components/safety';
-import WhatsNew from './components/whatsNew';
 import store from '../../shared/store/store';
 import {Provider} from 'react-redux';
-injectTap();
+
 function render(){
   let content = document.createElement('div');
   content.setAttribute('id', 'content');
@@ -22,17 +15,10 @@ function render(){
   ReactDOM.render(
     <Provider store={store}>
       <Router history={browserHistory}>
-        <Route path='/' component={App}>
-          <IndexRoute component={MeetingList} />
-          <Route path='how' component={How}/>
-          <Route path='when' component={When}/>
-          <Route path='safety' component={Safety} />
-          <Route path='new' component={WhatsNew} />
-        </Route>
         <Route path='/meeting/:meetingName' component={Meeting}/>
       </Router>
     </Provider>,
-      content
+    content
   );
 }
 
