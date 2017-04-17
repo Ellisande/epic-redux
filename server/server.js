efc115e8e8642bbc05746ac3219c47278ff150b9
Ellisande <justin.dragos@gmail.com>
2017-04-16 22:37:19 -0700

    Make server code more readable.

--- server/server.js
+++ server/server.js
@@ -17,22 +17,27 @@ const createServer = () => {
 
   const chatRoom = new Room();
 
-  const addUserToMeeting = (spark, room) => {
-    const remainingNames = room.store.getState().participants.map(p => p.name);
+  // List of things to do when adding a new user to the chat
+  const addUserToRoom = (spark, room) => {
+    const usedNames = room.store.getState().participants.map(p => p.name);
 
     const participant = {
-      name: determineName(remainingNames, spark.id),
+      name: determineName(usedNames, spark.id),
       id: spark.id,
     };
 
+    // Add the socket connection to our chat room
     room.addSpark(spark);
+    // Tell everyone that the person joined
     room.dispatch(addParticipant(participant));
-
+    // Send the initial state to the new connection
     spark.write(joinMeeting(room.store.getState()));
   };
 
   primus.on('disconnection', function(spark){
+    // Tell everyone the person left
     chatRoom.dispatch(removeParticipant(spark.id));
+    // Remove the connection from our room
     chatRoom.removeSpark(spark);
   });
 
@@ -44,8 +49,12 @@ const createServer = () => {
       });
     };
 
+    // When we get data from the socket
+    // Dispatch it to the store
     attachEvents(spark);
-    addUserToMeeting(spark, chatRoom);
+
+    // A new user connected, add them to our chat room
+    addUserToRoom(spark, chatRoom);
   });
 
   app.get(/^(?!primus).+/, (req, res) => res.sendFile(`${process.cwd()}/assets/index.html`));
