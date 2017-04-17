f314d1d4c65738c4bee66bee692f2f4c96bae778
Ellisande <justin.dragos@gmail.com>
2017-04-16 22:54:11 -0700

    More server code cleanup.

--- server/room.js
+++ server/room.js
@@ -1,6 +1,9 @@
 import {createStore} from '../shared/store/store';
 import {applyMiddleware} from 'redux';
 import {loggingMiddleware} from './middleware';
+import determineName from './utils';
+import {joinMeeting, addParticipant, removeParticipant} from '../shared/actions';
+
 
 class Room {
   constructor(roomType){
@@ -13,6 +16,9 @@ class Room {
     };
     const meetingMiddleware = applyMiddleware(loggingMiddleware, sendActionsMiddlware);
     this.store = createStore(meetingMiddleware);
+  }
+  getState(){
+    return this.store.getState();
   }
   send(action){
     this.sparks.forEach(spark => spark.write(action));
@@ -28,6 +34,29 @@ class Room {
   }
   removeSpark(spark){
     this.sparks = this.sparks.filter(currentSpark => currentSpark.id !== spark.id);
+  }
+  getAvailableName(defaultName){
+    const usedNames = this.getState().participants.map(p => p.name);
+    return determineName(usedNames, defaultName);
+  }
+  addUser(spark){
+    // Create a new chat paricipant
+    const participant = {
+      name: this.getAvailableName(spark.id),
+      id: spark.id,
+    };
+
+    // Add the socket connection to our chat room
+    this.addSpark(spark);
+    // Tell everyone that the person joined
+    this.dispatch(addParticipant(participant));
+    // Send the initial state to the new connection
+    spark.write(joinMeeting(this.getState()));
+  }
+  removeUser(spark){
+    this.dispatch(removeParticipant(spark.id));
+    // Remove the connection from our room
+    this.removeSpark(spark);
   }
 };
 
--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import {createServer as createHttpServer} from 'http';
 import PrimusServer from 'primus';
-import {joinMeeting, addParticipant, removeParticipant} from '../shared/actions';
-import determineName from './utils';
 import Room from './room';
 
 const createServer = () => {
@@ -17,44 +15,23 @@ const createServer = () => {
 
   const chatRoom = new Room();
 
-  // List of things to do when adding a new user to the chat
-  const addUserToRoom = (spark, room) => {
-    const usedNames = room.store.getState().participants.map(p => p.name);
-
-    const participant = {
-      name: determineName(usedNames, spark.id),
-      id: spark.id,
-    };
-
-    // Add the socket connection to our chat room
-    room.addSpark(spark);
-    // Tell everyone that the person joined
-    room.dispatch(addParticipant(participant));
-    // Send the initial state to the new connection
-    spark.write(joinMeeting(room.store.getState()));
-  };
-
   primus.on('disconnection', function(spark){
-    // Tell everyone the person left
-    chatRoom.dispatch(removeParticipant(spark.id));
-    // Remove the connection from our room
-    chatRoom.removeSpark(spark);
+    // Remove the user from the chat room
+    chatRoom.removeUser(spark);
   });
 
   primus.on('connection', function (spark) {
-    const attachEvents = sparkToAttach => {
-      sparkToAttach.on('data', action => {
-        action.userId = spark.id;
-        chatRoom.dispatch(action);
-      });
-    };
 
     // When we get data from the socket
     // Dispatch it to the store
-    attachEvents(spark);
+    // This is the heart of everything thats going on here
+    spark.on('data', action => {
+      action.userId = spark.id;
+      chatRoom.dispatch(action);
+    });
 
     // A new user connected, add them to our chat room
-    addUserToRoom(spark, chatRoom);
+    chatRoom.addUser(spark);
   });
 
   app.get(/^(?!primus).+/, (req, res) => res.sendFile(`${process.cwd()}/assets/index.html`));
