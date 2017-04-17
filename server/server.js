7aef773f480ca3f84c885db9b6b389edee64662f
Ellisande <justin.dragos@gmail.com>
2017-04-16 22:29:10 -0700

    Remove the multiple room concept from the server side.

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 import express from 'express';
 import {createServer as createHttpServer} from 'http';
 import PrimusServer from 'primus';
-import {joinMeeting, addParticipant, removeParticipant, setRoomName} from '../shared/actions';
-import _ from 'lodash';
+import {joinMeeting, addParticipant, removeParticipant} from '../shared/actions';
 import determineName from './utils';
 import Room from './room';
 
@@ -16,7 +15,7 @@ const createServer = () => {
     transformer: 'engine.io',
   });
 
-  let rooms = {};
+  const chatRoom = new Room();
 
   const addUserToMeeting = (spark, room) => {
     const remainingNames = room.store.getState().participants.map(p => p.name);
@@ -24,7 +23,6 @@ const createServer = () => {
     const participant = {
       name: determineName(remainingNames, spark.id),
       id: spark.id,
-      host: false
     };
 
     room.addSpark(spark);
@@ -34,34 +32,20 @@ const createServer = () => {
   };
 
   primus.on('disconnection', function(spark){
-    if(spark.query.room) {
-      const roomForUser = _.find(rooms, room => room.store.getState().participants.find(p => p.id === spark.id));
-      if(roomForUser){
-        roomForUser.dispatch(removeParticipant(spark.id));
-        roomForUser.removeSpark(spark);
-      }
-    }
+    chatRoom.dispatch(removeParticipant(spark.id));
+    chatRoom.removeSpark(spark);
   });
 
   primus.on('connection', function (spark) {
-    if(spark.query.room){
-      const roomName = spark.query.room;
-      if(!rooms[roomName]){
-        rooms[roomName] = new Room(undefined);
-        rooms[roomName].store.dispatch(setRoomName(roomName));
-      }
-      const currentRoom = rooms[roomName];
+    const attachEvents = sparkToAttach => {
+      sparkToAttach.on('data', action => {
+        action.userId = spark.id;
+        chatRoom.dispatch(action);
+      });
+    };
 
-      const attachEvents = sparkToAttach => {
-        sparkToAttach.on('data', action => {
-          action.userId = spark.id;
-          currentRoom.dispatch(action);
-        });
-      };
-
-      attachEvents(spark);
-      addUserToMeeting(spark, currentRoom);
-    }
+    attachEvents(spark);
+    addUserToMeeting(spark, chatRoom);
   });
 
   app.get(/^(?!primus).+/, (req, res) => res.sendFile(`${process.cwd()}/assets/index.html`));
