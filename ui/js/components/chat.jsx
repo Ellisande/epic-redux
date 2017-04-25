ab5bd0066c0d958fdc1151d4d8ff9c4a95756f85
Ellisande <justin.dragos@gmail.com>
2017-04-24 21:56:11 -0700

    Make setting user name a little different.

--- ui/js/components/chat.jsx
+++ ui/js/components/chat.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Message from './message';
 import {connect} from 'react-redux';
-import {sendMessage, joinChat} from '../../../shared/actions';
+import {sendMessage, addParticipant, joinChat} from '../../../shared/actions';
 import _ from 'lodash';
 import {findUser} from '../../../shared/store/utils';
 
@@ -9,14 +9,15 @@ class Chat extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      newMessage: undefined
+      newMessage: undefined,
+      userName: Math.floor(Math.random() * 10000) + '',
     };
-
     this.postTopic = this.post.bind(this);
     this.updatePostTopic = this.updatePostTopic.bind(this);
   }
-  componentWillMount() {
-    this.props.setUserName(Math.floor(Math.random() * 10000));
+  componentDidMount() {
+    this.props.setUserId(this.state.userName);
+    this.props.addUser(this.state.userName);
   }
   updatePostTopic(e) {
     const newMessage = _.get(e, 'target.value');
@@ -24,6 +25,9 @@ class Chat extends Component {
   }
   post(e) {
     e.preventDefault();
+    if (!this.props.user) {
+      return;
+    }
     if (!_.isEmpty(this.state.newMessage)) {
       this.props.sendMessage(this.state.newMessage, this.props.user.name);
       this.setState({newMessage: undefined});
@@ -55,7 +59,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = (dispatch) => ({
   sendMessage: (newMessage, userName) => dispatch(sendMessage(newMessage, userName)),
-  setUserName: userName => dispatch(joinChat({userId: userName, participants: [{ id: userName, name: userName }]})),
+  addUser: userName => dispatch(addParticipant({id: userName, name: userName})),
+  setUserId: userId => dispatch(joinChat({userId})),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Chat);
